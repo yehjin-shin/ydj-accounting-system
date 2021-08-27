e5f5842f032be2a38289bbfe76954c3eb4396d56
Shin Yehjin <68529301+yehjin-00@users.noreply.github.com>
2021-08-28 03:25:47 +0900

    210828

--- app/app.py
+++ app/app.py
@@ -49,9 +49,9 @@ class Receipt:
     def make_use_list(self):
         df = self.raw.copy()
 
-        df['영수증'] = self.get_id(df['영수증 (사진 파일로 업로드)'])
+        df['영수증'] = self.get_id(df['영수증 업로드'])
         df['개수'] = [len(v) for v in df['영수증']]
-        df = df.drop('영수증 (사진 파일로 업로드)', axis=1)
+        df = df.drop('영수증 업로드', axis=1)
         self.real_receipt = sum(df['개수'][df['실물영수증 여부']=='실물영수증 O'])
 
         df = df[['프로그램명', '인원', '계정사유', '적요', '결제일자', '금액', '가맹점명','개수']]
@@ -71,11 +71,11 @@ class Receipt:
         df = self.raw.copy()
 
         # 필요한 column만 남기기
-        df = df[['사용 RA','인원','결제일자','프로그램명','금액', '업체선정사유', '품명/수량/금액','영수증 (사진 파일로 업로드)']]
+        df = df[['사용 RA','인원','결제일자','프로그램명','금액', '업체선정사유', '품명/수량/금액','영수증 업로드']]
 
         # id만 list로 뽑아내는 과정
-        df['영수증'] = self.get_id(df['영수증 (사진 파일로 업로드)'])
-        df = df.drop('영수증 (사진 파일로 업로드)', axis=1)
+        df['영수증'] = self.get_id(df['영수증 업로드'])
+        df = df.drop('영수증 업로드', axis=1)
 
         # 원하는 꼴로 변형
         df['인원'] = [str(v)+" 명" for v in df['인원']]
@@ -155,8 +155,8 @@ class Receipt:
 
     # 사진 링크에서 id 뽑아내는 메소드
     def get_id(self, url_list):
-        url_list = [url+";" for url in url_list]
-        pattern = re.compile('{}(.*?){}'.format(re.escape('id='), re.escape(';')))
+        url_list = [url+"," for url in url_list]
+        pattern = re.compile('{}(.*?){}'.format(re.escape('id='), re.escape(',')))
         return [pattern.findall(url) for url in url_list] 
 
     # 숫자를 000,000,000 꼴로 바꾸는 정규식
@@ -175,7 +175,7 @@ class Program:
         btn_height = 2
 
         self.input_btn = tk.Button(win)
-        self.input_btn.config(width=btn_width, height=btn_height, text='파일(csv, excel)을 업로드해주세요', command=self.input_df)
+        self.input_btn.config(width=btn_width, height=btn_height, text='파일(xlsx, csv)을 업로드해주세요', command=self.input_df)
         self.input_btn.pack()
 
         self.path_btn = tk.Button(win)
@@ -194,17 +194,35 @@ class Program:
         quit_btn.config(width=btn_width, height=btn_height, text='종료', command=quit)
         quit_btn.pack()
 
-        win.mainloop() # 창 And 실행
+        self.info = tk.Tk() # 창 생성
+        self.info.geometry("300x100") # 창 크기
+        self.info.title("Information") # 제목
+        self.info.option_add("*Font", "맑은고딕 12") # 전체 폰트
+
+        win.mainloop()
+
+        # info_quit_btn = tk.Button(info)
+        # info_quit_btn.config(width=btn_width, height=btn_height, text='종료', command=quit)
+        # quit_btn.pack()
 
 
     def input_df(self):
-        filepath = filedialog.askopenfilename(title="파일을 선택하세요.", filetypes=(("csv files", "*.csv"), ("xlsx files", "*.xlsx")))   
+        filepath = filedialog.askopenfilename(title="파일을 선택하세요.", filetypes=(("xlsx files", "*.xlsx"), ("csv files", "*.csv")))   
         self.receipt = Receipt(filepath)
         self.receipt.make_use_list()
         self.receipt.make_receipt_df()
 
         show_path = '/'.join(filepath.split('/')[-3:])
         self.input_btn.config(text=f'업로드 완료\n{show_path}')
+
+        whole_num = len(self.receipt.use_list_df)
+        real_num = self.receipt.real_receipt
+        info_text = f"전체 영수증 개수: {whole_num} 개\n실물 영수증 개수: {real_num} 개"
+
+        info_label = tk.Label(self.info, text=info_text, width=20, height=5)
+        info_label.pack()
+
+        self.info.mainloop()
 
     def input_path(self):
         self.folder = filedialog.askdirectory(title="저장할 위치를 선택하세요.")
@@ -220,5 +238,15 @@ class Program:
             self.progress.set((i+1)/num)
             self.progress_bar.update()
         self.start_btn.config(text='제작이 완료되었습니다')
+
+    def num_receipt_info(self):
+        whole_num = len(self.receipt.use_list_df)
+        real_num = self.receipt.real_receipt
+        info_text = f"전체 영수증 개수: {whole_num} 개\n실물 영수증 개수: {real_num} 개"
+
+        info_label = tk.Label(self.info, text=info_text, width=25, height=5)
+        info_label.pack()
+
+        info.mainloop()
 
 Program()
